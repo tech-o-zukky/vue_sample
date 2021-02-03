9022d0c51bccb05e8cdb5e2a4e4686ddfc687b82
tech-o-zukky <tech.o.zukky@gmail.com>
2021-02-03 14:44:03 +0900

    ToDo処理作成

--- script/script_todo.js
+++ script/script_todo.js
@@ -25,6 +25,27 @@ new Vue ({
     },
     resetText() {
       this.text = '';
+    },
+    deleteTodo(id) {
+      const index = this.getIndexBy(id);
+      this.todos.splice(index, 1);
+    },
+    toggleIsDone(id) {
+      const index = this.getIndexBy(id);
+      this.todos[index].isDone = !this.todos[index].isDone;
+    },
+    getIndexBy(id) {
+      const filteredTodo = this.todos.filter(todo => todo.id === id)[0];
+      const index = this.todos.indexOf(filteredTodo);
+      return index;
+    },
+  },
+  computed: {
+    doneTodo() {
+      return this.todos.filter( todo => todo.isDone === true);
+    },
+    incompleteTodo() {
+      return this.todos.filter( todo => todo.isDone === false);
     }
   }
 });
